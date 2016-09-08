6b7dc94870386e77c889d28715be985574ba14d0
Louis de Charson <ltdc2121@hotmail.fr>
2016-09-08 13:46:23 +0200

    cleaning the house

--- routes/quandl.js
+++ routes/quandl.js
@@ -24,24 +24,6 @@ var assert = require('assert'),
     https = require('https');
 
 
-// function buildHMTL(arr,name) {
-
-//     var header = '<title>SDMX API for EViews / '+ name +'</title>';
-//     var body = '';
-//     var table ='';
-//     var theader1 = '<th>Dates</th><th>'+name+'</th>';
-//     var tbody = '';
-
-//     arr.forEach(function(it,ind) {
-//         tbody += '<tr><td>'+it[0]+'</td><td style="align:center">'+it[1]+'</td></tr>';
-//     });
-
-//     var myHtml = '<!DOCTYPE html>' + '<html><header>' + header + '</header><body>' + '<table>' + '<thead>'  + '<tr>' + theader1 + '</tr></thead>' + '<tbody>' + tbody + '</tbody>'  +'</table>' + '</body></html>';
-//     return myHtml;
-    
-// }
-
-
 function buildHMTL(arr,nameSeries,nameDataset) {
 
     var header = '<title>SDMX API for EViews / '+ nameDataset +'</title>';
@@ -66,57 +48,6 @@ function buildHMTL(arr,nameSeries,nameDataset) {
     return myHtml;
     
 }
-
-// exports.getSeries = function(req,res) {
-
-//     var arrSeries = req.params.series.split('+');
-//     var series = req.params.series;
-//     var dataset = req.params.dataset;
-//     var apiKey = req.params.apiKey;
-
-//     if (dataset === "ICE") {
-
-//         arrSeries.forEach(function(it,ind) {
-//             var myPath = '/api/v3/datasets/'+ dataset + '/' + it + '.json?api_key='+apiKey;
-//             var options = {
-//                 hostname: 'www.quandl.com',
-//                 port: 443,
-//                 path: myPath,
-//                 headers: {
-//                     'connection': 'keep-alive'
-//                 }
-//             };
-//             https.get(options, function(result) {
-//                 if (result.statusCode >=200 && result.statusCode < 400) {
-//                     var xml = '';
-//                     result.on('data', function(chunk) {
-//                         xml += chunk;
-//                     });
-//                     result.on('end',function() {
-//                         var json = JSON.parse(xml);
-//                         var nameSeries = json.dataset.column_names[4];
-//                         var mySeries = [];
-//                         json.dataset.data.forEach(function(item,indice) {
-//                             mySeries.push([item[0],item[4]]);
-//                         });
-//                         res.send(buildHMTL(mySeries.reverse(),nameSeries));
-//                     });
-//                 } else {
-//                     if (result.statusCode === 429) {
-//                         var msg = "Too many requests";
-//                     } else {
-//                         var msg = "";
-//                     }
-//                     var e = "Error: "+result.statusCode + ' | '+ msg  + ' | Headers '+JSON.stringify(result.headers) + ' | ';
-//                     res.send(e);
-//                 };
-//             });
-//         });
-//      } else {
-//         res.send('Not yet supported');
-//      }
-// };
-
 
 exports.getSeries = function(req,res) {
 
