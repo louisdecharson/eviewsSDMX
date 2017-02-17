82433daf105f6f34e1c550a2b223763c7ea0599e
Louis de Charson <ltdc2121@hotmail.fr>
2017-02-17 14:41:13 +0100

    Google Analytics

--- routes/buildHTML.js
+++ routes/buildHTML.js
@@ -12,6 +12,9 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+const gA = "<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-92058229-1', 'auto'); ga('send', 'pageview');</script>";
+
+
 // mini-function used to remove 'CL_' to the name of a dimension when retrieving codelist.
 function sliceCL(str) {
     if (str.substring(0,3) == "CL_") {
@@ -63,7 +66,7 @@ exports.dataFlow = function(data,service) {
         tbody += '<td class="desc">'+ item[3] + '</td>';
         tbody += '</tr>';
     });
-    var myHtml = '<!DOCTYPE html>' + '<html><head>' + header + jQuery + bootstrap + css + '</head><body>' + body + '<table class="table table-condensed table-hover">' + '<thead>'  + '<tr>' + theader + '</tr>' + '</thead>' + '<tbody class="list">' + tbody + '</tbody>'  +'</table></div>' + listJS  + jsforList + '</body></html>';
+    var myHtml = '<!DOCTYPE html>' + '<html><head>' + header + jQuery + bootstrap + css + '</head><body>' + body + '<table class="table table-condensed table-hover">' + '<thead>'  + '<tr>' + theader + '</tr>' + '</thead>' + '<tbody class="list">' + tbody + '</tbody>'  +'</table></div>' + listJS  + jsforList + gA + '</body></html>';
     return myHtml;
 };
 
@@ -195,7 +198,7 @@ exports.makeTable = function(vTS,title,authParams){
         tbody += '</tr>';
         i ++;
     };
-    var myHtml = '<!DOCTYPE html>' + '<html><header>' + header + '</header><body>' + '<table>' + '<thead>'  + '<tr>' + theader1 + '</tr>' + '<tr>' + theader2 + '</tr>'  + '</thead>' + '<tbody class="list">' + tbody + '</tbody>'  +'</table>' + '</body></html>';
+    var myHtml = '<!DOCTYPE html>' + '<html><header>' + header + '</header><body>' + '<table>' + '<thead>'  + '<tr>' + theader1 + '</tr>' + '<tr>' + theader2 + '</tr>'  + '</thead>' + '<tbody class="list">' + tbody + '</tbody>'  +'</table>' + gA + '</body></html>';
     
     return myHtml;
 };
@@ -268,7 +271,7 @@ exports.detailDataset = function(service,vTS,dataSet,arr,errorDatasetTooBig) {
     }
                  
     
-    var myHtml = '<!DOCTYPE html>' + '<html><header>' + header + bootstrap + css+ '</header><body>' + body + error + searchBar  + tableDef + '<thead>'  + '<tr>' + theader + '</tr>' + '</thead>' + '<tbody class="list">' + tbody + '</tbody>'  +'</table></div>' + listJS + jsforList + '</body></html>';
+    var myHtml = '<!DOCTYPE html>' + '<html><header>' + header + bootstrap + css+ '</header><body>' + body + error + searchBar  + tableDef + '<thead>'  + '<tr>' + theader + '</tr>' + '</thead>' + '<tbody class="list">' + tbody + '</tbody>'  +'</table></div>' + listJS + jsforList + gA + '</body></html>';
     
     return myHtml;
 };
@@ -290,7 +293,7 @@ exports.codeList = function (codes,title_dim) {
         tbody += '<td style="min-width:100px">' + item['Name'][item['Name'].length-1]['_']+'</td></tr>';
         
     });
-    var myHtml = '<!DOCTYPE html>' + '<html><header>' + header + bootstrap + css + '</header><body>' + '<table class="table table-hover table-condensed">' + '<thead>'  + '<tr>' + theader + '</tr>' + '</thead>' + '<tbody>' + tbody + '</tbody>'  +'</table>' + '</body></html>';
+    var myHtml = '<!DOCTYPE html>' + '<html><header>' + header + bootstrap + css + '</header><body>' + '<table class="table table-hover table-condensed">' + '<thead>'  + '<tr>' + theader + '</tr>' + '</thead>' + '<tbody>' + tbody + '</tbody>'  +'</table>' + gA + '</body></html>';
     return myHtml;
 };
 
