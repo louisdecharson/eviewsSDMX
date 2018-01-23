7b92c17e2052798e143e6012600b49453d334e7e
Louis de Charson <ltdc2121@hotmail.fr>
2018-01-24 00:23:12 +0100

    correct bugs'

--- routes/explore.js
+++ routes/explore.js
@@ -24,8 +24,8 @@ const providers = require('./providers.json');
 exports.getProviders = function(req,res) {
     var body = '<body><span class="badge badge-pill badge-primary badge-home"><a href="/">Back</a></span><div class="listProviders"><h1 style="text-align:center;">Providers</h1><ul class="providers">';
     Object.keys(providers).forEach(function(provider,index) {
-        var title = '<li class="provider" data-toggle="collapse" data-target="#'+ provider + '">' + provider,
-            content = '<div class="collapse content" id="'+ provider + '">';
+        var title = '<li class="provider" data-toggle="collapse" data-target="#'+ provider + '">' + providers[provider].name,
+            content = '<div class="collapse content" id="'+ provider + '"><p class="key">Provider key:<span class="badge badge-dark">'+ provider +'</span></p>';
         if (providers[provider].native === "True") {
             title += '<span class="badge badge-pill badge-primary">native</span>';
             content += '<h4>List datasets</h4><p>The list of available datasets is available at <a href="/' + provider + '/dataflow">/'+ provider.toLowerCase() + '/dataflow</a></p>';
@@ -33,7 +33,13 @@ exports.getProviders = function(req,res) {
             content += '<p>Url to retrieve data is:<br/><strong><center><code><span class="url">http://sdmx.herokuapp.com/</span><span class="providercolor">'+ provider.toLowerCase()  +'</span>/<span class="resource">resource</span>/<span class="resource_id">resource_id</span></code></center></strong><br/>';
             content += '<strong class="resource">resource</strong><ul style="margin-bottom: 5px;"><li><code>dataset</code>: for retreiving a dataset.</li><li><code>series</code>: for retreiving a timeseries.</li></ul>';
             content += '<strong class="resource_id">resource_id</strong> is the <strong>id</strong> of either the dataset or the timeseries.';
-        } else {
+        } else if (provider === "OECD") {
+            title += '<span class="badge badge-pill badge-secondary">non-native</span>';
+            content += '<h4>List datasets</h4><p>The list of available datasets is available at <a href="/' + provider + '/dataflow">/'+ provider.toLowerCase() + '/dataflow</a></p>';
+            content += '<h4>Get data</h4><div>';
+            content += '<p>Url to retrieve data is:<br/><strong><center><code><span class="url">http://sdmx.herokuapp.com/</span><span class="providercolor">'+ provider.toLowerCase()  +'</span>/<span class="resource">dataset_id</span>/<span class="resource_id">resource_id</span></code></center></strong><br/>';
+            content += '<strong class="resource_id">resource_id</strong> is the <strong>id</strong> of either the dataset or the timeseries.';
+        } else  {
             title += '<span class="badge badge-pill badge-secondary">non-native</span>';
             if (providers[provider].apiKey === "True") {
                 content += '<p><span style="color:#da3749; font-weight: bold;">&#9888; An API key is required to access the data.</span><p/>';
